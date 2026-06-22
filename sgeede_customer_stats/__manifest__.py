dcc16565ee25a6451de252348c14dc01bcd02f02
Sen <jassonjasson17@gmail.com>
2026-06-22 15:24:54 +0700

    Add Sgeede Customer Stats v19

---
+++ sgeede_customer_stats/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Sale Customer Statistics',
+    'version': '19.0.1.0.0',
+    'category': 'Sales/Sales',
+    'summary': 'Customer sales statistics on contact records',
+    'description': """
+Display quotation and sales order statistics on customer contact records.
+    """,
+    'author': 'SGEEDE',
+    'website': 'https://www.sgeede.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'contacts',
+        'sale',
+        'sale_management',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
